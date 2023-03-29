eb278f6d38372d4c7406d6125802c43e63c2681b
GreXLin85 <grexlin85@gmail.com>
2023-03-29 18:49:33 +0300

    Method check added

--- src/pages/api/shorten.ts
+++ src/pages/api/shorten.ts
@@ -2,21 +2,24 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { Redis } from '@upstash/redis'
 
-
 type Data = {
   name: string
 }
-
-
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ name: 'Method Not Allowed' })
+  }
+
   const redis = Redis.fromEnv()
 
   const { url } = req.body
+
   const id = Math.random().toString(36).slice(2)
   await redis.set(id, url)
-  res.status(200).json({ name: id })
+
+  return res.status(200).json({ name: id })
 }
